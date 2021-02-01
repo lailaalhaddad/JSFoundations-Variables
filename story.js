9300697901e357da0da66d9019a8cc2e76b73d02
lailaalhaddad <lalhaddad95@gmail.com>
2021-02-01 20:42:41 +0300

    updated

--- story.js
+++ story.js
@@ -1,4 +1,6 @@
 let name = ("Laila");
 let age = (25);
 let hobby = ("reading books");
-console.log("Mrs." + " " + name + ", " + "in 33 years, you will be" + " " + (age + 33) + " " + "years old." + " " + "By then you'll have decades of" + " " + hobby + " " + "practice.");
+// console.log("Mrs." + " " + name + ", " + "in 33 years, you will be" + " " + (age + 33) + " " + "years old." + " " + "By then you'll have decades of" + " " + hobby + " " + "practice."); // another way
+console.log(`Mrs. ${name} in 33 years, you will be ${age + 33} years old. 
+By then you'll have decades of ${hobby} pratice.`);
