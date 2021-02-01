7ce3e62fd12e731bba7da65858f8463b3bd01b00
lailaalhaddad <lalhaddad95@gmail.com>
2021-02-01 19:39:31 +0300

    task three done

---
+++ story.js
@@ -0,0 +1,4 @@
+let name = ("Laila");
+let age = (25);
+let hobby = ("reading books");
+console.log("Mrs." + " " + name + ", " + "in 33 years, you will be" + " " + (age + 33) + " " + "years old." + " " + "By then you'll have decades of" + " " + hobby + " " + "practice.");
